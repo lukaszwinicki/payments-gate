d469027d267183c8a232f5c675cbed582c69b781
lukaszwinicki <lukaszwinicki@gmail.com>
2025-12-11 22:51:33 +0000

    Dodano HasApiTokens  do modelu User. Wyłączono supports_credentials w konfiguracji CORS. Ustawiono wygaśnięcie tokena Sanctum na 20 minut.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::get('/', function () {
     return auth()->check()
         ? (auth()->user()->hasRole('admin') ? redirect('/admin') : redirect('/user'))
         : redirect()->route('login');
 });
-
 
 require __DIR__ . '/auth.php';
